c7d192a377e3edb4e9d83fa2dd06de48a6aa6822
unhurried <unhurried@users.noreply.github.com>
2022-12-31 19:10:05 +0900

    Add colums to Todo table.

--- ent/schema/todo.go
+++ ent/schema/todo.go
@@ -1,6 +1,9 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
 
 // Todo holds the schema definition for the Todo entity.
 type Todo struct {
@@ -9,7 +12,11 @@ type Todo struct {
 
 // Fields of the Todo.
 func (Todo) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("title").NotEmpty(),
+		field.String("category").NotEmpty(),
+		field.String("content").Optional(),
+	}
 }
 
 // Edges of the Todo.
